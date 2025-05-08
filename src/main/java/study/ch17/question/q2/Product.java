a3c5ef34cc3108597f3b60043f3cd73d408d5329
Kyumg Hyun <jake.roh2000@gmail.com>
2025-05-08 17:33:08 +0900

    java 스트림 심화2

---
+++ src/main/java/study/ch17/question/q2/Product.java
@@ -0,0 +1,11 @@
+package study.ch17.question.q2;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+  private String name;
+  private String category;
+  private double price;
+}
